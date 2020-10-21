9e7b69571465d6f19770324d3b7f12ae58413324
Martin Stabenfeldt <martin@stabenfeldt.net>
2020-10-21 13:06:08 +0200

    Better check for verifying that the transporter has started.
    (cherry picked from commit d30c55a3203beb60163b2027a170211b95c9cfab)
    
    	transporter/run.sh

--- transporter/run.sh
+++ transporter/run.sh
@@ -6,13 +6,17 @@ printf "\nIN TRANSPORTER SETUP\n"
 
 MONGO=`ping -c 1 mongo-rs0-1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 ES=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MARKER=/scripts/.transpoter_on
+MARKER=/scripts/.TRANSPORTER-STARTED
 
 # Cleanup from previous runs
 if test -f "$MARKER"; then
     rm $MARKER
 fi
 
+# Cleanup from previous runs
+if test -f "$MARKER"; then
+  rm $MARKER
+fi
 
 cd $GOPATH; mkdir pkg
 mkdir -p src/github.com/compose; cd src/github.com/compose
@@ -31,5 +35,11 @@ go build -a ./cmd/transporter/...
 
 /scripts/wait-until-mongodb-started.sh
 
+# Signal that we're starting
+printf "=====================================\n\n"
+printf Touching file
+printf "=====================================\n\n"
 touch $MARKER
-./transporter run --config ./config.yaml ./mongo-es.js
+
+cd /transporter
+transporter run --config ./config.yaml ./mongo-es.js
